1b9a1a35c46e0ef3a2cfec84a502ae041922de77
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-03-10 12:32:17 +0545

    modified table to return appropriate date with @property

--- Tables/models.py
+++ Tables/models.py
@@ -1,9 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import User
-   
+from django.db import models
+
+
 class DiningTable(models.Model):
     table_name = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True,editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self):
         return self.table_name
