4009238adec84ef3dd7a8cbb91fd946becd4c220
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-03-13 16:12:00 +0545

    added OrderConsumer with connection handling and preloaded data

--- Order/consumers.py
+++ Order/consumers.py
@@ -21,3 +21,28 @@ class CloseCodes:
     INSUFFICIENT_FUNDS = 4400
     ITEM_OUT_OF_STOCK = 4401
     QUANTITY_EXCEEDED = 4402
+
+class OrderConsumer(WebsocketConsumer):
+    def connect(self):
+        self.order_id=self.scope['url_route']['kwargs']['order_id']
+        try:
+            Order.objects.get(id=self.order_id)
+            self.orderGroup=f'order_{self.order_id}'
+            self.channel_layer.group_add(self.orderGroup,self.channel_name)
+        except Order.DoesNotExist:
+            self.close(code =CloseCodes.ORDER_NOT_FOUND,reason='Invalid Id provided')
+        self.accept()
+        self.send_preloaded_data()
+            
+    def send_preloaded_data(self):
+        Orderdata=Order.objects.get(id=self.order_id)
+        serializers=OrderSerializer(Orderdata)
+        self.send(
+            text_data=json.dumps(serializers.data)
+        )
+
+    def recieve(self):
+        pass
+    
+    def diconnect(self):
+        pass
