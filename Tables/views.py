169c325dfed8f28a04ce108dfdd8538af372780b
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-04-05 21:36:00 +0545

    added capacity for each table and status of the table (Ocuupied and Avaialble)

--- Tables/views.py
+++ Tables/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes , authentication_classes
-from rest_framework.permissions import IsAuthenticated 
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+    authentication_classes,
+)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from decorator.role import role_required
