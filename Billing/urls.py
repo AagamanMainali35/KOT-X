44f23ccee2f29a08be3768336d29cfc1e4f84bae
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-04-05 21:32:10 +0545

    migartion file for billing

--- Billing/urls.py
+++ Billing/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path("create-bill/", create_bill, name="create-bill"),  # POST
     path("<int:id>/", getOrders),
-    path("create-bill", create_bill, name="create-bill"),  # POST
 ]
