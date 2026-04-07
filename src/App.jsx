9ed1b5a362178955f5d009536eb7012b13b61f0b
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-04-07 11:23:09 +0545

    added URL or table and menu management pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import Test from './pages/menu';
+import Menu from './pages/menu';
 import AdminDashboard from "./pages/AdminDashboard"
 import MenuManagement from "./pages/MenuManagement"
+import TableManagement from "./pages/Table"
 import Billing from "./pages/Billing"
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Order" element={<Test />} />
+        <Route path="/Order" element={<Menu />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/menu" element={<MenuManagement />} />
+        <Route path="/admin/table" element={<TableManagement />} />
         <Route path="/billing" element={<Billing />} />
       </Routes>
     </Router>
